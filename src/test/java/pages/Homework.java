6fe588e3d198e3a73106789a846c4a8a22af7dcd
Mikon141 <tobimm7@gmail.com>
2022-10-20 16:21:08 +0200

    Add new files

---
+++ src/test/java/pages/Homework.java
@@ -0,0 +1,16 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Homework {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\mikon\\Downloads\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.newegg.com/");
+        System.out.println(driver.getTitle());
+    }
+
+}
